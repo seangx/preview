97e72f332b2bbee5b0f4a383fbd1fa712a9b514a
Joker <seangx@qq.com>
2018-01-31 21:33:15 +0800

    UPD

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 window._CCSettings = {
-    "platform": "web-desktop",
+    "platform": "web-mobile",
     "groupList": [
         "default"
     ],
@@ -481,6 +481,6 @@ window._CCSettings = {
             "977b+CqBJAa4IHS+iruUCh"
         ]
     },
-    "orientation": "landscape",
+    "orientation": "",
     "debug": true
 };
