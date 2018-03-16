706d5f43f7c3a8c3c2fd006bb56dcf6634d4b710
Joker <seangx@qq.com>
2018-03-16 10:34:13 +0800

    upd

--- src/assets/scripts/defines.js
+++ src/assets/scripts/defines.js
@@ -1,1 +1,14 @@
-var defines={};defines.timeScale=1,defines.addSpeed=240,defines.config={gameConfig:"/config/game-config"},defines.poolNodeType={coin:"coin",stub:"stub",enemy:"enemy"},window.defines=defines;
+var defines = {};
+defines.timeScale = 1;
+defines.addSpeed = 240;
+defines.config = {
+    gameConfig: '/config/game-config'
+};
+
+defines.poolNodeType = {
+    'coin': 'coin',
+    'stub': 'stub',
+    'enemy': 'enemy'
+};
+
+window.defines = defines;
